8389258325865f4023814a273883bd731d6b702c
Maanrifa <dev.manrif@gmail.com>
2020-01-06 11:42:17 +0100

    Migration to C++17

--- src/core/io/command/internal/ConfigCommand.cpp
+++ src/core/io/command/internal/ConfigCommand.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "ConfigCommand.hpp"
-#include <experimental/filesystem>
+#include <filesystem>
 
 ConfigCommand::ConfigCommand()
         : Command("config", "c", "Set the config file absolute path.", "-c \"/home/foo/bar.txt\"", true)
@@ -11,7 +11,7 @@ ConfigCommand::ConfigCommand()
 
 char *ConfigCommand::validate(char *value, std::vector<Command *> &options, TypeUtil::ErrorCallback& on_error)
 {
-    if (!std::experimental::filesystem::is_regular_file(value)) {
+    if (!std::filesystem::is_regular_file(value)) {
         on_error("The file \"" + std::string(value) + "\" is not a valid file.\nPlease give a valid file full path.");
     }
     return value;
@@ -22,5 +22,5 @@ const char* ConfigCommand::get_value() const
     if (value_) {
         return value_;
     }
-    return std::experimental::filesystem::is_regular_file(DEFAULT_FILE_1) ? DEFAULT_FILE_1 : DEFAULT_FILE_2;
+    return std::filesystem::is_regular_file(DEFAULT_FILE_1) ? DEFAULT_FILE_1 : DEFAULT_FILE_2;
 }
