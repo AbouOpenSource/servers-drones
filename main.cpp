14b237d09d79e9ec30d2f3d61240c0061d193322
Maanrifa <dev.manrif@gmail.com>
2020-01-21 15:07:13 +0100

    Bug fixed

--- main.cpp
+++ main.cpp
@@ -1,41 +1,46 @@
 #include <vector>
 #include "src/core/io/command/internal/ConfigCommand.hpp"
 #include "src/core/io/command/CommandParser.hpp"
-#include "src/core/io/reader/FileStream.hpp"
-#include "src/model/Matrix.hpp"
 #include "src/gui/Window.hpp"
+#include "src/core/ServiceContainer.hpp"
+#include "src/gui/drawable/Circumscribe.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 
-    // Init arguments
+    // Command Service
     std::vector<Command*> commands(Command::all_internals());
     CommandParser command_parser(commands, true);
-    CommandWrapper command_wrapper = command_parser.parse(argc, argv);
+    CommandContainer command_container = command_parser.parse(argc, argv);
 
-    // Read servers data
+    // Input Service
+    InputManager input_manager;
+
+    // Window Service
+    Window window(argc, argv, &input_manager);
+
+    // Instantiating service container
+    ServiceContainer* service_container = ServiceContainer::get_instance();
+    service_container->register_service(&command_container);
+    service_container->register_service(&input_manager);
+    service_container->register_service(&window);
+
+    /*Read servers data
     FileStream in_stream(command_wrapper.get_value("c"), {";", true});
 
     auto reader_interface = in_stream.reader();
-    auto write_interface = in_stream.writer();
 
-    // Usage example
+    Usage example
     reader_interface.read([&write_interface] (const unsigned int row, const unsigned int col, const std::string& value) {
-        // Write the same data
-        write_interface.write(row, col, value);
-    }, [&write_interface] () {
-        // Store on disk
-        write_interface.persist();
+    }, [] () {
     });
-  
-    Matrix<int> matrix(10);
-    //std::cout << matrix.determinant();
+    */
 
-    //std::cout << matrix;
+    Circumscribe circumscribe;
 
-    Window window("Triangulation", argc, argv);
+    window.addDrawable(&circumscribe);
     window.start();
 
     return 0;
